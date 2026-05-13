a78197fd67dbb3736ecdd2a86356bcc4065e2980
Super Zombi <super.zombi.yt@gmail.com>
2026-05-13 16:38:49 +0100

    Update app.jsx

--- apps/start_menu/app.jsx
+++ apps/start_menu/app.jsx
@@ -1,5 +1,5 @@
 ({}) => {
-    console.log("here")
+    console.log(window.AppManager.registeredApps)
     return (
         <div className="p-2">
             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quam eos debitis dolores, deserunt perspiciatis, praesentium atque nisi reprehenderit rem quidem illo vitae fugiat unde natus nam! Pariatur, qui ea.</h3>
